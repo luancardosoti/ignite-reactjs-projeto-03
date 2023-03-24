6c47f63f48e263a570ffc5a0527f8edac098d006
= <luancardosoti@gmail.com>
2023-03-24 15:37:35 -0300

    feat: adicionando analytics do next

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { TransactionsProvider } from './contexts/TransactionsContext'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Analytics } from '@vercel/analytics/react'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Analytics />
 
       <TransactionsProvider>
         <Transactions />
